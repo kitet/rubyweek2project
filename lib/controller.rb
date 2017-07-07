31dbe2a2956a14a84687b52ecf27bc8009a3129a
kitet <kitetnet@gmail.com>
2017-07-07 14:15:44 +0300

    added methods to controller class to retrieve all words. added method to save definitions to a word

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,17 +1,36 @@
 class Controller
+	@@words=[]
 	def initialize()
+	end
+
+	def self.new_word(word)
+		@@words.push(word)
+	end
+
+	def self.get_all_words()
+		@@words
 	end
 end
 
 class Word
+	@@definitions=[]
 	def initialize(word) 
 		@word=word
 	end
 
+	def add_definition(definition)
+		define=Definition.new(definition)
+		@id=@@definitions.length().+(1)
+		@@definitions.push(define)
+	end
+
+	def all_definitions()
+		@@definitions
+	end
 end
 
 class Definition
-	def initialize(definition)
-		@definition=definition
+	def initialize(string)
+		@definition=string
 	end
 end
