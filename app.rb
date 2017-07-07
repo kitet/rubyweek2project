d3b94b68e7a9544548497babada1e03f9d297de6
kitet <kitetnet@gmail.com>
2017-07-07 16:18:58 +0300

    added post method to capture when new word form is submitted, and redirect to success.erb

--- app.rb
+++ app.rb
@@ -4,5 +4,22 @@ require './lib/controller.rb'
 also_reload('lib/**/*.rb')
 
 get '/' do
+	@listedwordobjects=Controller.get_all_words()
 	erb :index
+end
+
+get '/new_word' do
+	erb :new_word
+end
+
+post '/new_word' do
+	word=params['word']
+	newword=Word.new(word)
+	Controller.new_word(newword)
+	erb :success
+end
+
+get '/word/:id' do
+	@word=Controller.find_by_id(params.fetch('id').to_i())
+	erb :word
 end
