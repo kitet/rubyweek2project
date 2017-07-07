15115821264b70c1ee97ef18ab9df4d117525ccc
kitet <kitetnet@gmail.com>
2017-07-07 15:18:27 +0300

    Added methods to the Controller class in rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -4,6 +4,7 @@ class Controller
 	end
 
 	def self.new_word(word)
+		word.id=@@words.length().+(1)
 		@@words.push(word)
 	end
 
@@ -11,26 +12,30 @@ class Controller
 		@@words
 	end
 end
-
+#class to hold Word object
 class Word
+	attr_accessor(:word,:id)
 	@@definitions=[]
 	def initialize(word) 
 		@word=word
+		@id=0
 	end
 
 	def add_definition(definition)
-		define=Definition.new(definition)
-		@id=@@definitions.length().+(1)
-		@@definitions.push(define)
+		mydef=Definition.new(definition)
+		mydef.id=@@definitions.length().+(1)
+		@@definitions.push(mydef)
 	end
 
 	def all_definitions()
 		@@definitions
 	end
 end
-
+#class to hold Definition object
 class Definition
+	attr_accessor(:definition,:id)
 	def initialize(string)
 		@definition=string
+		@id=0
 	end
 end
