b879894e8834be60bdd75df5b4666d6e4665973b
kitet <kitetnet@gmail.com>
2017-07-07 13:22:41 +0300

    Created controller to control word and definition class objects

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,17 @@
+class Controller
+	def initialize()
+	end
+end
+
 class Word
+	def initialize(word) 
+		@word=word
+	end
+
 end
 
 class Definition
+	def initialize(definition)
+		@definition=definition
+	end
 end
